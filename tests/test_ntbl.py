2fa81b1374d2d895e283e94d87539a2ce46d548d
Elijah Wilson <eli@noteable.io>
2021-07-14 15:40:12 -0400

    tests

--- tests/test_ntbl.py
+++ tests/test_ntbl.py
@@ -1,3 +1,4 @@
+import json
 import traceback
 from unittest import mock
 
@@ -37,28 +38,7 @@ def context():
 def test_datasets_push(push_mock, input_path, expected_path, runner, context):
     result = runner.invoke(datasets_push, input_path.split(' '), obj=context)
     assert result.exit_code == 0, ''.join(traceback.format_exception(*result.exc_info))
-    push_mock.assert_called_with(expected_path, timeout=(0.5, 3600.0))
-
-
-@mock.patch(
-    'noteable_magics.planar_ally_client.api.DatasetFileSystemAPI.push',
-    return_value=UserMessage(message='Success'),
-)
-@pytest.mark.parametrize(
-    'input_path,expected_path,timeout',
-    [
-        ('foobar', 'foobar/', 125.0),
-        ('foo bar', 'foo bar/', 125.0),
-        ('foo bar/baz bak', 'foo bar/baz bak', 125.0),
-        ('foo/bar', 'foo/bar', 125.0),
-        ('foo/bar/', 'foo/bar/', 125.0),
-        ('', '/', 125.0),
-    ],
-)
-def test_datasets_push_with_timeout(push_mock, input_path, expected_path, timeout, runner, context):
-    result = runner.invoke(datasets_push, ['-t', timeout] + input_path.split(' '), obj=context)
-    assert result.exit_code == 0, ''.join(traceback.format_exception(*result.exc_info))
-    push_mock.assert_called_with(expected_path, timeout=(0.5, timeout))
+    push_mock.assert_called_with(expected_path)
 
 
 @mock.patch(
@@ -79,25 +59,4 @@ def test_datasets_push_with_timeout(push_mock, input_path, expected_path, timeou
 def test_datasets_pull(push_mock, input_path, expected_path, runner, context):
     result = runner.invoke(datasets_pull, input_path.split(' '), obj=context)
     assert result.exit_code == 0, ''.join(traceback.format_exception(*result.exc_info))
-    push_mock.assert_called_with(expected_path, timeout=(0.5, 3600.0))
-
-
-@mock.patch(
-    'noteable_magics.planar_ally_client.api.DatasetFileSystemAPI.pull',
-    return_value=UserMessage(message='Success'),
-)
-@pytest.mark.parametrize(
-    'input_path,expected_path,timeout',
-    [
-        ('foobar', 'foobar/', 125.0),
-        ('foo bar', 'foo bar/', 125.0),
-        ('foo bar/baz bak', 'foo bar/baz bak', 125.0),
-        ('foo/bar', 'foo/bar', 125.0),
-        ('foo/bar/', 'foo/bar/', 125.0),
-        ('', '/', 125.0),
-    ],
-)
-def test_datasets_pull_with_timeout(push_mock, input_path, expected_path, timeout, runner, context):
-    result = runner.invoke(datasets_pull, ['-t', timeout] + input_path.split(' '), obj=context)
-    assert result.exit_code == 0, ''.join(traceback.format_exception(*result.exc_info))
-    push_mock.assert_called_with(expected_path, timeout=(0.5, timeout))
+    push_mock.assert_called_with(expected_path)
