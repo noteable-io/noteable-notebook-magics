573f530682487c3fb709d9a2c76ca50b456690b4
Elijah Wilson <eli@noteable.io>
2021-08-18 15:33:58 -0400

    test

--- tests/test_ntbl.py
+++ tests/test_ntbl.py
@@ -4,7 +4,7 @@ from unittest import mock
 import pytest
 from click.testing import CliRunner
 
-from noteable_magics.ntbl import NTBLMagic, datasets_pull, datasets_push
+from noteable_magics.ntbl import NTBLMagic, change_log_level, datasets_pull, datasets_push
 from noteable_magics.planar_ally_client.api import DatasetOperationStream
 from noteable_magics.planar_ally_client.types import (
     FileProgressUpdateContent,
@@ -78,3 +78,12 @@ def test_datasets_pull(input_path, expected_path, runner, context):
         result = runner.invoke(datasets_pull, input_path.split(' '), obj=context)
         assert result.exit_code == 0, ''.join(traceback.format_exception(*result.exc_info))
         pull_mock.assert_called_with(expected_path)
+
+
+def test_change_log_level(runner, context):
+    with mock.patch(
+        "noteable_magics.planar_ally_client.api.PlanarAllyAPI.change_log_level", return_value=None
+    ) as change_mock:
+        result = runner.invoke(change_log_level, ["--app-level", "DEBUG"], obj=context)
+        assert result.exit_code == 0, ''.join(traceback.format_exception(*result.exc_info))
+        change_mock.assert_called_with(app_log_level="DEBUG", ext_log_level=None)
