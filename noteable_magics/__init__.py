84636b584a3fb5ad45d81ef6221e84fe84e4cc9a
James Robinson <james.robinson@noteable.io>
2022-04-27 13:27:48 -0400

    Always blacklist bigquery -- legacy connection to bigquery wont be using datasources.

--- noteable_magics/__init__.py
+++ noteable_magics/__init__.py
@@ -2,6 +2,9 @@ import pkg_resources
 
 __version__ = pkg_resources.get_distribution("noteable_magics").version
 
+from sql.run import add_commit_blacklist_dialect
+
+
 from .data_loader import NoteableDataLoaderMagic, get_local_db_connection
 from .datasources import bootstrap_datasources
 from .logging import configure_logging
@@ -10,7 +13,14 @@ from .ntbl import NTBLMagic
 
 def load_ipython_extension(ipython):
 
-    # initialize the noteable local (sqlite) database connection
+    # Initialize any remote datasource connections
+    bootstrap_datasources()
+
+    # Always prevent sql-magic from trying to autocommit bigquery,
+    # for the legacy datasource support for Expel and whomever.
+    add_commit_blacklist_dialect('bigquery')
+
+    # Initialize the noteable local (sqlite) database connection
     get_local_db_connection()
 
     configure_logging(False, "INFO", "DEBUG")
