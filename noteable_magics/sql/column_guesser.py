8b14064c3b95f4d3b6b536546fbfdf14cc866fa8
James Robinson <james.robinson@noteable.io>
2022-07-27 15:49:45 -0400

    Strip whitespace from empty lines.

--- noteable_magics/sql/column_guesser.py
+++ noteable_magics/sql/column_guesser.py
@@ -7,7 +7,7 @@ makes guesses about the role of each column for plotting purposes
 
 class Column(list):
     """Store a column of tabular data; record its name and whether it is numeric"""
-    
+
     is_quantity = True
     name = ""
 
