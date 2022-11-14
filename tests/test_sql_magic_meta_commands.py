72d4f51053964ee2ab1a579a771867ee44aa9da6
James Robinson <james.robinson@noteable.io>
2022-11-14 16:25:03 -0500

    more isortery

--- tests/test_sql_magic_meta_commands.py
+++ tests/test_sql_magic_meta_commands.py
@@ -1,10 +1,9 @@
 import re
 from typing import Optional, Tuple
 
-from IPython.display import HTML
-
 import pandas as pd
 import pytest
+from IPython.display import HTML
 from sqlalchemy.engine.reflection import Inspector
 
 from noteable_magics.sql.connection import Connection
